df0a64cfa64c929e2d0aebc257d7232c8dddcb3c
Blaise Leone <bpl7@geneseo.edu>
2023-12-01 10:11:34 -0500

    “ANY_MASSAGE”

---
+++ website stuff.R
@@ -0,0 +1,10 @@
+git config --list
+git config --global user.email "bpl7@geneseo.edu"
+git config --global user.name "Blaisel22"
+
+# install.packages("usethis")
+usethis::create_github_token()
+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
+
+# install.packages("gitcreds")
+gitcreds::gitcreds_set()
