675646a97408625607b3828e95b899ec70f803e0
Blaise Leone <bpl7@geneseo.edu>
2023-12-06 13:53:12 -0500

    ANY_MASSAGE

--- website stuff.R
+++ website stuff.R
@@ -1,6 +1,6 @@
-git config --list
-git config --global user.email "bpl7@geneseo.edu"
-git config --global user.name "Blaisel22"
+#git config --list
+#git config --global user.email "bpl7@geneseo.edu"
+#git config --global user.name "Blaisel22"
 
 # install.packages("usethis")
 usethis::create_github_token()
@@ -8,3 +8,8 @@ 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
 
 # install.packages("gitcreds")
 gitcreds::gitcreds_set()
+
+# altctrl I
+library(tidyverse)
+library(hrbrthemes)
+library(skimr)
